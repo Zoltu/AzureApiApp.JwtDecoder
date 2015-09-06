7b00c10095de50639b140f625f331710b1818872
Micah Zoltu <micah@zoltu.net>
2015-09-05 19:37:28 -0700

    First pass.

--- apiapp/Controllers/JwtDecoderController.cs
+++ apiapp/Controllers/JwtDecoderController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Diagnostics.Contracts;
+using System.IdentityModel.Tokens;
+using System.ServiceModel.Security.Tokens;
 using System.Web.Http;
 
 namespace Zoltu.RepoCreator.Controllers
@@ -8,11 +10,50 @@ namespace Zoltu.RepoCreator.Controllers
 	{
 		[HttpGet]
 		[Route("api/JwtDecoder")]
-		public String Get()
+		public String Get(String jwtTokenOrAuthorizationHeader, String base64EncodedSecret, String validAudience, String validIssuer)
 		{
+			Contract.Requires(jwtTokenOrAuthorizationHeader != null);
+			Contract.Requires(base64EncodedSecret != null);
 			Contract.Ensures(Contract.Result<String>() != null);
 
-			return "Hello API App!";
+			var jwtEncodedString = GetJwtTokenFromPossiblyHeader(jwtTokenOrAuthorizationHeader);
+			ValidateJwtWithHs256(jwtEncodedString, base64EncodedSecret, validAudience, validIssuer);
+
+			var payload = new JwtSecurityToken(jwtEncodedString).RawPayload;
+			Contract.Assume(payload != null);
+			return payload;
 		}
+
+		private static String GetJwtTokenFromPossiblyHeader(String jwtTokenOrAuthorizationHeader)
+		{
+			Contract.Requires(jwtTokenOrAuthorizationHeader != null);
+			Contract.Ensures(Contract.Result<String>() != null);
+
+			if (!jwtTokenOrAuthorizationHeader.StartsWith("bearer "))
+				return jwtTokenOrAuthorizationHeader;
+
+			return jwtTokenOrAuthorizationHeader.Substring("bearer ".Length);
+		}
+
+		private static void ValidateJwtWithHs256(String encodedJwt, String base64EncodedSecret, String validAudience, String validIssuer)
+		{
+			var secret = Base64UrlEncoder.DecodeBytes(base64EncodedSecret);
+			ValidateJwtWithHs256(encodedJwt, secret, validAudience, validIssuer);
+		}
+
+		private static void ValidateJwtWithHs256(String encodedJwt, Byte[] secret, String validAudience, String validIssuer)
+		{
+			var tokenValidationParameters = new TokenValidationParameters
+			{
+				ValidAudience = validAudience,
+				ValidIssuer = validIssuer,
+				IssuerSigningToken = new BinarySecretSecurityToken(secret),
+				ValidateAudience = validAudience != null,
+				ValidateIssuer = validIssuer != null,
+			};
+			SecurityToken securityToken;
+			new JwtSecurityTokenHandler().ValidateToken(encodedJwt, tokenValidationParameters, out securityToken);
+		}
+
 	}
 }
